e938cf3cc093e64bfc48b382d37dd9de45f2be22
Sonia <soniagha@thoughtworks.com>
2011-09-10 09:22:01 +0530

    my 1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PairingMatrix::Application.config.secret_token = '6ad1fb8db7f2f3c694f269e801d9fabf8c18679573680fd56475ce0b6bd9824a8e2ce3b96b2460ceed9fa1d0fb6cc2edb9edd2c4f602bc4bbcb640826da86d34'
