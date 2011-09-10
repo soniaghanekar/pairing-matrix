566ad07fa7931487c2961a8653c6677695dc21ad
Sonia <soniagha@thoughtworks.com>
2011-09-10 22:59:59 +0530

    added partial for tabs and able to view count based on names

--- app/controllers/pairing_details_controller.rb
+++ app/controllers/pairing_details_controller.rb
@@ -1,9 +1,15 @@
 class PairingDetailsController < ApplicationController
 
   def get_count
-    @PairingDetails=PairingDetails.find_by_pair1_id_and_pair2_id(params[:pair1_id],params[:pair2_id])
-    @count=@PairingDetails.number_of_times_paired
-    #render :template=> 'get/count'
+    id1=get_id_from_employee_details(params[:pair1_id])
+    id2=get_id_from_employee_details(params[:pair2_id])
+    @count=PairingDetails.find_by_pair1_id_and_pair2_id(id1,id2).number_of_times_paired
+
+  end
+
+  def get_id_from_employee_details(name)
+     id=EmployeeDetails.find_by_name(name).id
+    id
   end
 
 end
